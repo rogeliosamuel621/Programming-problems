3564761f250fea8918bfefb77ecba7726efb5519
rogeliosamuel621 <rogeliosamuel621@gmail.com>
2021-01-03 15:54:48 -0700

    singly linked list

--- ts/src/linkedList.ts
+++ ts/src/linkedList.ts
@@ -1,3 +1,35 @@
-const myName: string = 'Rogelio';
+interface NewNode {
+    value: number,
+    next: NewNode | null
+}
+interface LinkedList {
+    head: NewNode;
+    tail: NewNode;
+    length: number;
+}
 
-console.log(`My name is ${myName}`);
+class NewNode implements NewNode {
+    constructor(value: number){
+        this.value =  value
+        this.next = null;
+    }
+}
+
+class LinkedList implements LinkedList { 
+    constructor(value: number){
+        this.head = {value: value, next: null}
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value: number): LinkedList{
+        const newNode = new NewNode(value)
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+
+        return this;
+    }
+}
+
+const linkedList = new LinkedList(1);
